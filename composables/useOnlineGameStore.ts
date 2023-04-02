42fe682eb08ea8ecccae64fd48cee384d21ba90f
Atsuya Yamagami <my2502@icloud.com>
2023-04-03 00:37:52 +0900

    add a necessary initialization

--- composables/useOnlineGameStore.ts
+++ composables/useOnlineGameStore.ts
@@ -3,10 +3,9 @@ import { Turn } from "@/constants/game";
 export const useOnlineGameStore = () => {
   const room = useState<string>('room');
   const myTurn = useState<Turn>('myTurn');
-  const playerNames = useState<{
-    player1: string;
-    player2: string;
-    player3: string;
-  }>('playerNames');
+  const playerNames = useState(
+    'playerNames',
+    () => ({ player1: '', player2: '', player3: '' })
+  );
   return { room, myTurn, playerNames };
 };
